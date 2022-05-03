06f2836ec1bca1882d91e742925165361462d6fa
Nikola <stojanovicn129@gmail.com>
2022-05-03 14:41:12 +0200

    First commit

---
+++ display_yolo_detections.py
@@ -0,0 +1,75 @@
+import argparse
+import json
+import os
+
+import cv2
+from draw_util BoundingBox, import draw_2D_box, init_colors
+
+
+def parse_args():
+    """Function for parsing input arguments
+
+    Returns:
+        (object): object containing passed arguments
+    """
+    parser = argparse.ArgumentParser(description='Neural network training',
+                                    formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--json_file',
+                        default = "/home/machinecansee/Programming/darknet/result.json", type = str,
+                        help = 'specify path to Darknet results json file')
+    parser.add_argument('--save_dir',
+                        default = "output/real_photos/", type = str,
+                        help = 'specify folder for saving images')
+    parser.add_argument('--classes_file',
+                        default = "/home/machinecansee/Programming/mcs/dataset/new_dataset_yolo/vehicle_classes.txt", type = str,
+                        help = 'specify path to file with class names')
+    parser.add_argument('--confidence',
+                        default = 0.0, type = float,
+                        help = 'set condfidence threshold')
+    parsed_args = parser.parse_args()
+    parsed_args.classes = read_txt_file(parsed_args.classes_file)
+    parsed_args.colors = init_colors(len(parsed_args.classes))
+    return parsed_args
+
+def read_txt_file(filepath):
+    """ Read lines from text file.
+
+    Args:
+        filepath (str): Path to file
+
+    Returns:
+        (list): lines from file
+    """
+    return [line.strip() for line in open(filepath).readlines()]
+
+def parse_detections(args):
+    with open(args.json_file) as f:
+        images_data = json.load(f)
+
+    if not os.path.isdir(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    for image in images_data:
+        img_og = cv2.imread(image["filename"])
+        print('Processing image:', image["filename"])
+        image_name = image["filename"][image["filename"].rindex(os.path.sep)+1:]
+        img_cpy = img_og.copy()
+        img_height, img_width, _ = img_og.shape
+        object_list = image["objects"]
+        for count, obj in enumerate(object_list):
+            if obj["confidence"]*100 >= args.confidence:
+                bbox_message = "%d. %s (%.2f)" % (count, obj["name"], obj["confidence"]*100)
+                center_x = obj["relative_coordinates"]["center_x"] * img_width
+                center_y = obj["relative_coordinates"]["center_y"] * img_height
+            
+                width = obj["relative_coordinates"]["width"] * img_width
+                height = obj["relative_coordinates"]["height"] * img_height
+                bbox = BoundingBox((center_x - width / 2), (center_y - height / 2), (center_x + width / 2), (center_y + height / 2))
+                draw_2D_box(img_cpy, bbox.to_numpy_array(), args.colors, args.classes.index(obj["name"]), bbox_message, show_label=True)
+        cv2.imwrite(args.save_dir + image_name, img_cpy)
+
+    print("Done!")
+
+if __name__ == "__main__":
+    args = parse_args()
+    parse_detections(args)
---
+++ draw_util.py
@@ -0,0 +1,143 @@
+import colorsys
+import random
+
+import cv2
+import numpy as np
+
+class BoundingBox:
+    """ Util class to present the 2d bounding dox of the object in one class by remembering two 
+        corners (upper left and bottom right) """
+
+    def __init__(self, xmin, ymin, xmax, ymax):
+        """ Constructor of the 2d bounding box class
+
+        Args:
+            xmin (float): x value of the upper left corner of 2d bounding box
+            ymin (float): y value of the upper left corner of 2d bounding box
+            xmax (float): x value of the bottom right corner of 2d bounding box
+            ymax (float): y value of the bottom right corner of 2d bounding box
+        """
+
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+
+def draw_3D_box(image, corners):
+    """ Drawing a 3D bounding box around the 3D detected object based on 8 corners that 
+        defines it position in the 3D world
+
+    Args:
+        image (array): image to draw on 3D bounding box
+        points (array): 8 corners of the detected object
+    """
+
+    # converting corners into numpy elements
+    corners = corners.astype(np.int)
+    # drawing 4 lines that defines the depth of the detected object using green color
+    for i in range(4):
+        first_corner = corners[2 * i]
+        second_corner = corners[2 * i + 1]
+        cv2.line(image, (first_corner[0], first_corner[1]), (second_corner[0], second_corner[1]), 
+                 (0, 255, 0), 2)
+    
+    # drawing 8 lines that defines the width and length of the detected object using green color
+    for i in range(8):
+        first_corner = corners[i]
+        second_corner = corners[(i + 2) % 8]
+        cv2.line(image, (first_corner[0], first_corner[1]), (second_corner[0], second_corner[1]),
+                 (0, 255, 0), 2)
+
+    # drawing X sign to define the front side of the detected object using red color
+    cv2.line(image, tuple(corners[0]), tuple(corners[7]), (0, 0, 255), 2)
+    cv2.line(image, tuple(corners[1]), tuple(corners[6]), (0, 0, 255), 2)
+
+    # drawing X sign to define the upper side of the detected object using purple color
+    cv2.line(image, tuple(corners[3]), tuple(corners[7]), (255, 0, 255), 2)
+    cv2.line(image, tuple(corners[5]), tuple(corners[1]), (255, 0, 255), 2)
+
+def draw_yolo_bbox(image, bboxes, classes, show_label=True):
+    """Draw bounding box for Yolo inference.
+
+    Args:
+        image (obj): image to draw on
+        bboxes (obj): array of bounding boxes to draw
+        classes (dict): classes dict
+        show_label (bool, optional): Show class labels. Defaults to True.
+
+    Returns:
+        (obj): processed image
+    """
+    
+    num_classes = len(classes)
+    image_h, image_w, _ = image.shape
+    colors = init_colors(num_classes)
+
+    out_boxes, out_scores, out_classes, num_boxes = bboxes
+    for i in range(num_boxes[0]):
+        if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > num_classes: continue
+        coor = out_boxes[0][i]
+        coor[0] = int(coor[0] * image_h)
+        coor[2] = int(coor[2] * image_h)
+        coor[1] = int(coor[1] * image_w)
+        coor[3] = int(coor[3] * image_w)
+
+        fontScale = 0.5
+        score = out_scores[0][i]
+        class_ind = int(out_classes[0][i])
+        bbox_color = colors[class_ind]
+        bbox_thick = int(0.6 * (image_h + image_w) / 600)
+        c1, c2 = (coor[1], coor[0]), (coor[3], coor[2])
+        cv2.rectangle(image, c1, c2, bbox_color, bbox_thick)
+
+        if show_label:
+            bbox_mess = '%s: %.2f' % (classes[class_ind], score)
+            t_size = cv2.getTextSize(bbox_mess, 0, fontScale, thickness=bbox_thick // 2)[0]
+            c3 = (c1[0] + t_size[0], c1[1] - t_size[1] - 3)
+            cv2.rectangle(image, c1, (np.float32(c3[0]), np.float32(c3[1])), bbox_color, -1) #filled
+
+            cv2.putText(image, bbox_mess, (c1[0], np.float32(c1[1] - 2)), cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale, (0, 0, 0), bbox_thick // 2, lineType=cv2.LINE_AA)
+    return image
+
+def init_colors(classes_num=15):
+    hsv_tuples = [(1.0 * x / classes_num, 1., 1.) for x in range(classes_num)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+    print(colors)
+    random.seed(0)
+    random.shuffle(colors)
+    random.seed(None)
+
+    return colors
+
+def draw_2D_box(image, rectangle_points, colors=None, class_index=None, bbox_message=None, show_label=False, font_scale=0.5):
+    """ Drawing a 2D bounding box around the 3D detected object based on 4 corners that defines it 
+        position in the 3D world
+
+    Args:
+        image (array): image to draw on 3D bounding box
+        rectangle_points (array): 4 points of rectangle of the detected object
+        index (int, optional): index of the detected vehicle on the image. Defaults to None.
+        bbox_message (str, optional): label name of the detected object. Defaults to None.
+    """
+
+    rectangle_points = np.ceil(rectangle_points).astype(np.int)
+    image_height, image_width, _ = image.shape
+    # create tuples for vehicle bounding box
+    vehicle_upper_left, vehicle_bottom_right = (rectangle_points[0], rectangle_points[1]), (rectangle_points[2], rectangle_points[3])
+    if colors is None:
+        bbox_color = (255, 0, 0)
+    else:
+        bbox_color = colors[class_index]
+    bbox_thick = int(0.6 * (image_height + image_width) / 600)
+    # drawing rectangle on the image based on the rectangle points
+    cv2.rectangle(image, vehicle_upper_left, vehicle_bottom_right, bbox_color, bbox_thick)
+    
+    if show_label:
+            t_size = cv2.getTextSize(bbox_message, 0, font_scale, thickness=bbox_thick // 2)[0]
+            text_bottom_right = (vehicle_upper_left[0] + t_size[0], vehicle_upper_left[1] - t_size[1] - 3)
+            cv2.rectangle(image, vehicle_upper_left, text_bottom_right, bbox_color, -1) #filled
+
+            cv2.putText(image, bbox_message, (vehicle_upper_left[0], vehicle_upper_left[1] - 2), cv2.FONT_HERSHEY_SIMPLEX,
+                        font_scale, (0, 0, 0), bbox_thick // 2, lineType=cv2.LINE_AA)
